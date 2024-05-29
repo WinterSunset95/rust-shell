b0dde2c40648d59974c7b5f7b920d38535259a92
WinterSunset95 <wintersunset95@gmail.com>
2024-05-29 23:44:31 +0530

    implemented type command for PATH variable

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
-
-    // Wait for user input
     // Infinite loop
     loop {
         print!("$ ");
@@ -34,23 +28,43 @@ fn command(input: String) -> String {
     let input: Vec<&str> = input.split_whitespace().collect();
 
     // Below is a vector of currently supported commands
+    // We will add more commands to this vector as we implement them
     let commands = vec!["echo", "exit", "type"];
 
     if input[0] == "echo" {
+
         // Command: echo
         // Join the arguments into a single string
         let output = input[1..].join(" ");
         return output;
+
     } else if input[0] == "type" {
+
         // Command: type
         // We might have more than one argument, so we join them into a single string
         let args = input[1..].join(" ");
+
         // Check if the command is supported
         if commands.contains(&args.as_str()) {
             return format!("{} is a shell builtin", args);
         } else {
+            // For non-builtins
+            // We gonna need to check the PATH environment variable
+            let path = env::var("PATH").unwrap();
+            let paths: Vec<&str> = path.split(":").collect();
+            for p in paths {
+                let file = format!("{}/{}", p, args);
+                // Check if the file is executable
+                if std::fs::metadata(file.clone()).is_ok() {
+                    return format!("{} is {}", args, file);
+                } else {
+                    continue;
+                }
+            }
+
             return format!("{} not found", args);
         }
+
     } else {
         return format!("{}: command not found", input[0]);
     }
